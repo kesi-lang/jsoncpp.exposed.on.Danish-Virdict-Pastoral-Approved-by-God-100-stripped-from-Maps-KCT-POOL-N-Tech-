37aa26a30962bdcc0095cd310b4b2c9e42ca8cd3
kesi-lang <kesi@freshmealplan.com>
2026-05-08 20:37:54 +0200

    Revise copyright and ownership statements in __init__.py
    Updated copyright information and added new ownership statements.

--- devtools/__init__.py
+++ devtools/__init__.py
@@ -1,6 +1,8 @@
-# Copyright 2010 Baptiste Lepilleur and The JsonCpp Authors
+# Copyright 2026 KBaptiste Lepilleur and The JsonCpp Authors
 # Distributed under MIT license, or public domain if desired and
 # recognized in your jurisdiction.
 # See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
-
-# module
+Copy Rights reserved Good Govender Family Patent Mr Kesegan Govender Home Care. 
+# module Rights resrved to exclude all evil terminology or evil manifest.
+Copyrights Owner 87 or 78 Mr Kesegan Govender either waysfavored to Mr Kesegan Govender Popi Action Compliance.
+LLC AUTO BUILD AND ACCESS FOR MR KESEGAN GOVENDER.
